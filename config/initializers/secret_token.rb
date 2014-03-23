d5f6a2b0c8741a59b276c91361a91134deabbeaa
Viktor <vnonov@gmail.com>
2014-03-23 10:14:00 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bidit::Application.config.secret_key_base = '4c467b0169b54da8e53c0ed2f211c56d777ed8b24a94b541aa4ec8cce3a6dcac27c4d4b6a73935c0b8b0add7efbed34b006dab4e85ef250e3e6b9783b726d4a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bidit::Application.config.session_store :cookie_store, key: '_bidit_session'
