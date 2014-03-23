42b75bb182daff1077cdc3a3d5ff5f34392a2655
Viktor <vnonov@gmail.com>
2014-03-23 16:53:36 +0200

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Item.delete_all
+
+Item.new(title: 'Kindle DX',
+         description: 'super ultra cool awesome ebook reader with brutally broken display',
+         start_price: 100.01,
+         end_date: Time.now + 1.month,
+         status: 'SELLING').save(validate: false)
+         
+Item.new(title: 'Gold Chain',
+         description: 'Dirt Nasty`s gold chain from 1980',
+         start_price: 590.01,
+         end_date: Time.now - 1.month,
+         status: 'SOLD').save(validate: false)
+
+Item.new(title: 'Painting',
+         description: 'beautiful painting by liybo chuka',
+         start_price: 590.01,
+         end_date: Time.now - 1.month,
+         status: 'NOTSOLD').save(validate: false)
+
+
